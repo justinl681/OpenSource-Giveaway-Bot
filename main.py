eeb14dfe6eb232a1c5ebe0f6adb4cb7c1617583c
justinl681 <59315005+justinl681@users.noreply.github.com>
2020-10-22 20:29:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ import datetime
 import asyncio
 from discord.ext import commands
 import time
+
+
 bot = commands.Bot(command_prefix='$')
+bot.remove_command('help')
 
 @bot.event
 async def on_ready():
@@ -15,11 +18,13 @@ async def on_ready():
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
-        embedVar = discord.Embed(title="Helping you out!", description="The correct way to start a giveaway is '$new (prize), (time to claim the prize in minutes)' or for the reroll command: '$reroll (message id)'", color=0x00ff00)
+        embedVar = discord.Embed(title="Helping you out!", description="The correct way to start a giveaway is '$new (prize for the giveaway), (time to claim the prize in minutes)' or for the reroll command: '$reroll (message id of the giveaway you want to reroll)'", color=0x00ff00)
         embedVar.add_field(name="Sidenote:", value="If you're having trouble with multiple-word prizes, use periods (.) instead of spaces.")
-        await ctx.send(embed=embedVar)   
+        await ctx.send(embed=embedVar)  
+
 
 @bot.command()
+@commands.cooldown(1, 10, commands.BucketType.user)
 @commands.has_permissions(administrator=True)
 async def new(ctx, prize, mins: int):
     end = datetime.datetime.utcnow().replace(microsecond=0) + datetime.timedelta(minutes=mins)
@@ -36,19 +41,23 @@ async def new(ctx, prize, mins: int):
     embedVar = discord.Embed(title=f"{winner.name} won:", description=prize.replace('.', ' '), color=0x00ff00)
     await mesg.edit(embed=embedVar)
 
+
 @bot.command()
+@commands.cooldown(1, 10, commands.BucketType.user)
 @commands.has_permissions(administrator=True)
 async def reroll(ctx, messageid):
     mesg = await ctx.fetch_message(messageid)
     users = await mesg.reactions[0].users().flatten()
     users.pop(users.index(bot.user))
     winner = random.choice(users)
-    await ctx.send(f"Reroll winner is: {winner.mention}!")
-    
+    embedVar = discord.Embed(title=f"Reroll!", description=f"{winner.name} won!")
+    await ctx.send(embed=embedVar)
+
 
 @bot.command()
+@commands.cooldown(1, 10, commands.BucketType.user)
 async def ghelp(ctx):
-    embedVar = discord.Embed(title="Helping you out!", description="The correct way to start a giveaway is '$new (prize), (time to claim the prize in minutes)' or for the reroll command: '$reroll (message id)'", color=0x00ff00)
+    embedVar = discord.Embed(title="Helping you out!", description="The correct way to start a giveaway is '$new (prize for the giveaway), (time to claim the prize in minutes)' or for the reroll command: '$reroll (message id of the giveaway you want to reroll)'", color=0xfffff)
     embedVar.add_field(name="Sidenote:", value="If you're having trouble with multiple-word prizes, use periods (.) instead of spaces.")
     await ctx.send(embed=embedVar)
 
